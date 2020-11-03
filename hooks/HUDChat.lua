4378cba378996ba4679909e55ac3b03ba9b1a4aa
Mako <masterjon902@gmail.com>
2020-11-02 19:15:56 -0600

    Update to change the mat config to fix map switching problem, also HUD fix

--- hooks/HUDChat.lua
+++ hooks/HUDChat.lua
@@ -1,43 +1,55 @@
-if not SystemFS:exists("mods/WolfHUD-master/mod.txt") then
-function HUDChat:_layout_output_panel()
-	local output_panel = self._panel:child("output_panel")
-
-	output_panel:set_w(self._output_width)
-
-	local line_height = HUDChat.line_height
-	local lines = 0
-
-	for i = #self._lines, 1, -1 do
-		local line = self._lines[i][1]
-		local icon = self._lines[i][2]
-
-		line:set_w(output_panel:w() - line:left())
-
-		local _, _, w, h = line:text_rect()
-
-		line:set_h(h)
-
-		lines = lines + line:number_of_lines()
+local bad_huds = {"WolfHUD-master","WolfHUD","Hotline Miami Hud","BL2HUD","PDTH Hud","SydneyHUD","VanillaHUD Plus","Holo","Void UI","NobleHUD"}
+local custom_hud = false
+for k, v in pairs(bad_huds) do
+	if SystemFS:exists("mods/"..v.."/mod.txt") then
+		custom_hud = true
 	end
+end
 
-	output_panel:set_h(line_height * math.min(6, lines))
+if custom_hud == true then
+	log("////Custom Hud Detected////")
+end
+if custom_hud == false then
+	function HUDChat:_layout_output_panel()
+		local output_panel = self._panel:child("output_panel")
 
-	local y = 0
+		output_panel:set_w(self._output_width)
 
-	for i = #self._lines, 1, -1 do
-		local line = self._lines[i][1]
-		local icon = self._lines[i][2]
-		local _, _, w, h = line:text_rect()
+		local line_height = HUDChat.line_height
+		local lines = 0
 
-		line:set_bottom(output_panel:h() - y)
+		for i = #self._lines, 1, -1 do
+			local line = self._lines[i][1]
+			local icon = self._lines[i][2]
 
-		if icon then
-			icon:set_top(line:top() + 1)
+			line:set_w(output_panel:w() - line:left())
+
+			local _, _, w, h = line:text_rect()
+
+			line:set_h(h)
+
+			lines = lines + line:number_of_lines()
 		end
 
-		y = y + h
-	end
+		output_panel:set_h(line_height * math.min(6, lines))
 
-	output_panel:set_bottom(self._input_panel:top())
-end
+		local y = 0
+
+		for i = #self._lines, 1, -1 do
+			local line = self._lines[i][1]
+			local icon = self._lines[i][2]
+			local _, _, w, h = line:text_rect()
+
+			line:set_bottom(output_panel:h() - y)
+
+			if icon then
+				icon:set_top(line:top() + 1)
+			end
+
+			y = y + h
+		end
+
+		output_panel:set_bottom(self._input_panel:top())
+		--log("////No Custom HUD Detected////")
+	end
 end
